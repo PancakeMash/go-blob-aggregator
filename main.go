764f59cc54af53d0818114b2feaeeffbf64eebf0
PancakeMash <arpandevofficial@gmail.com>
2025-10-09 21:01:23 +0100

    C: Added state to more accurately share login instance. Added cliCommand flexibility so new commands can be added an ran.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 )
 
 import "github.com/PancakeMash/go-blob-aggregator/internal/config"
@@ -13,15 +14,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = cfg.SetUser("mash")
+	appState := &state{config: &cfg}
+
+	cliCommands := &commands{}
+	cliCommands.m = make(map[string]func(*state, command) error)
+	cliCommands.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("not enough arguments were provided")
+	}
+
+	commandName := os.Args[1]
+	commandArgs := os.Args[2:]
+
+	err = cliCommands.run(appState, command{commandName, commandArgs})
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	updatedCfg, err := config.Read()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(updatedCfg)
+	fmt.Println(appState.config)
 }
